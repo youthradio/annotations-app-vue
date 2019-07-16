420f613c59bb5d6266742dee0c373f0b807f342b
Radames Ajna <radamajna@gmail.com>
2019-07-15 18:37:55 -0700

    cleaning up initial template

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,12 @@
 </template>
 
 <style lang="scss">
+@import url('https://yr.media/statics/yr-media-typography/yr-assistant-roboto.css');
+
+$font-family-sans-serif: 'Assistant', sans-serif;
+
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: $font-family-sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -21,35 +24,6 @@ html {
 *:before,
 *:after {
   box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  margin: 0px;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,14 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        annotations-app-vue
-      </h1>
-      <h2 class="subtitle">
-        My unreal Nuxt.js project
-      </h2>
-      <div class="links">
-        <div class="comments">
-          <ul>
-            <li v-for="comment in comments" :key="comment">
-              {{ comment.content }}
-            </li>
-          </ul>
-        </div>
-        <div class="text" v-html="text" />
-      </div>
-    </div>
+    <div class="text" v-html="text" />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 import COMMENTS_DATA from '~/data/data.json'
 
 export default {
-  components: {
-    Logo
-  },
+  components: {},
   computed: {
     comments() {
       return COMMENTS_DATA.comments
@@ -44,32 +23,8 @@ export default {
 <style lang="scss">
 .container {
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
