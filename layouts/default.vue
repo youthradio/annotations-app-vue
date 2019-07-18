679ee9f0838611c66c7735e4dc2b7bd431d60859
Radames Ajna <radamajna@gmail.com>
2019-07-17 18:12:53 -0700

    highlighting comments

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,38 +7,6 @@
 <style lang="scss">
 @import './gutenberg-config';
 
-@import '../node_modules/gutenberg-web-type/src/style/mixins/font-size';
-@import '../node_modules/gutenberg-web-type/src/style/mixins/line-height';
-@import '../node_modules/gutenberg-web-type/src/style/mixins/margin';
-@import '../node_modules/gutenberg-web-type/src/style/mixins/padding';
-@import '../node_modules/gutenberg-web-type/src/style/helpers/alignment';
-// @import '../node_modules/gutenberg-web-type/src/style/helpers/grid';
-@import '../node_modules/gutenberg-web-type/src/style/layout/aside';
-@import '../node_modules/gutenberg-web-type/src/style/layout/base';
-@import '../node_modules/gutenberg-web-type/src/style/layout/footer';
-@import '../node_modules/gutenberg-web-type/src/style/layout/header';
-@import '../node_modules/gutenberg-web-type/src/style/layout/main';
-@import '../node_modules/gutenberg-web-type/src/style/layout/section';
-@import '../node_modules/gutenberg-web-type/src/style/modules/anchor';
-@import '../node_modules/gutenberg-web-type/src/style/modules/article';
-@import '../node_modules/gutenberg-web-type/src/style/modules/audio';
-@import '../node_modules/gutenberg-web-type/src/style/modules/canvas';
-@import '../node_modules/gutenberg-web-type/src/style/modules/code';
-@import '../node_modules/gutenberg-web-type/src/style/modules/details';
-@import '../node_modules/gutenberg-web-type/src/style/modules/figure';
-@import '../node_modules/gutenberg-web-type/src/style/modules/form';
-@import '../node_modules/gutenberg-web-type/src/style/modules/heading';
-// @import '../node_modules/gutenberg-web-type/src/style/modules/horizontal-rule';
-@import '../node_modules/gutenberg-web-type/src/style/modules/image';
-@import '../node_modules/gutenberg-web-type/src/style/modules/list';
-@import '../node_modules/gutenberg-web-type/src/style/modules/navigation';
-@import '../node_modules/gutenberg-web-type/src/style/modules/progress';
-@import '../node_modules/gutenberg-web-type/src/style/modules/quote';
-@import '../node_modules/gutenberg-web-type/src/style/modules/table';
-@import '../node_modules/gutenberg-web-type/src/style/modules/template';
-@import '../node_modules/gutenberg-web-type/src/style/modules/text';
-@import '../node_modules/gutenberg-web-type/src/style/modules/video';
-
 // html {
 //   font-family: $font-family-sans-serif;
 //   font-size: 16px;
--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,32 @@
 </template>
 
 <script>
+import matchAll from 'string.prototype.matchall'
 import COMMENTS_DATA from '~/data/data.json'
 
 export default {
   components: {},
   computed: {
-    comments() {
+    comments () {
       return COMMENTS_DATA.comments
     },
-    text() {
+    filteredComments () {
+      return this.comments.map(e => e.content)
+    },
+    text () {
       return COMMENTS_DATA.text
-    }
-  }
+    },
+    getHTMLHighlights () {
+      const html = this.text
+      const result = this.filteredComments // clean strings to match on regex
+
+      const tags = Array.from(matchAll(result, /{([^}]+)\}/g)) // match all contetnt between {}
+      const regex = tags.map(e => e[1]).join('|')
+      const regexEscaped = regex.replace(/[-/\\^$*+?.()[\]{}]/g, '\\$&') // escape all caracters
+      const rg = new RegExp(regexEscaped, 'gi')
+      return html.replace(rg, '<span class="highlight">$&</span>') // find and replace with higlights
+    },
+  },
 }
 </script>
 
@@ -27,4 +41,10 @@ export default {
 //   justify-content: center;
 //   align-items: center;
 // }
+.container {
+  /deep/ .highlight {
+    font-weight: bolder;
+    background-color: rgba(255, 255, 0, 0.8);
+  }
+}
 </style>
