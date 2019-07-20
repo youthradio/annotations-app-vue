e60b0c9af0070962514346c08e3cb585579123b0
Radames Ajna <radamajna@gmail.com>
2019-07-19 18:15:55 -0700

    comments box with click events

--- components/CommentBox.vue
+++ components/CommentBox.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="box">
+  <div
+    :class="['box', isSelected? 'selected' : 'unselected' ]"
+    @click="clickEvent"
+  >
     {{ commentText }}
   </div>
 </template>
@@ -15,27 +18,58 @@ export default {
       type: String,
       default: null,
       require: true
+    },
+    commentId: {
+      type: Number,
+      default: null,
+      require: true
+    },
+    isSelected: {
+      type: Boolean,
+      default: false,
+      require: true
     }
   },
   data () {
     return {
     }
   },
+  computed: {
+
+  },
   watch: {
 
   },
-  mounted () {},
-  methods: {}
+  methods: {
+    clickEvent () {
+      this.$root.$emit('activeComment', {
+        commentId: this.commentId
+      })
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
+@import '~@/css/vars';
 
 .box {
-  border: 2px solid black;
+  border: 1px solid rgb(0, 0, 0);
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  background-color: white;
   margin-top: auto;
   margin-bottom: auto;
-  padding: 1rem;
+  max-width: 18em;
+  padding: 1em;
+  font-size: $scale / 2 * 1rem;
+}
+.selected {
+  transform: translate(-1em, -1em);
+  box-shadow: 2px 2px 5px black;
+  transition: all 300ms;
+}
+.unselected {
+  transition: all 300ms;
 }
 </style>
