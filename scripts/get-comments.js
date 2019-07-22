a5d6d4fbaa5e82d76ccccbafb066c853474799aa
Radames Ajna <radamajna@gmail.com>
2019-07-22 13:06:17 -0700

    commenting code

--- scripts/get-comments.js
+++ scripts/get-comments.js
@@ -8,21 +8,21 @@ async function getComments () {
   const goot = new Gootenberg()
   await goot.auth.jwt(credentials)
 
-  const comments = await goot.drive.comments(DOC_ID)
-  const textWithComments = await goot.drive.export(DOC_ID, 'text/plain')
+  const comments = await goot.drive.comments(DOC_ID) // get comments data from google DOC_ID
+  const textWithComments = await goot.drive.export(DOC_ID, 'text/plain') // get plain text data from google DOC_ID
   const text = textWithComments
-    .split('[a]')
+    .split('[a]') // split text from footer comments starting on [a] characters
     .slice(0, 2) // slice text from comments ref on bottom
-    .join('')
+    .join('') // remove footer and join remaining content
   const textCleaned = text.replace(/[\u200B-\u200D\uFEFF]/g, '')
   const textNoComments = textCleaned.replace(/\[.\]/g, '') // remove comments brackets
   const textHtml = textNoComments.replace(/^(.*)(\r\n|\r|\n)/gm, '<p>$&</p>') // wrap lines with <p></p> tags
 
   comments.forEach((comment, id) => {
     const regexEscaped = comment.quotedFileContent.value.replace(/[-/\\^$*+?.()[\]{}]/g, '\\$&') // escape all caracters
-    const regex = new RegExp(regexEscaped, 'gi')
-    const match = regex.exec(textHtml)
-    comment.position = match.index
+    const regex = new RegExp(regexEscaped, 'gi') // build regex
+    const match = regex.exec(textHtml) // match regex on html text
+    comment.position = match.index // return regex position for ordering comments
   })
   await fsPromises.writeFile(
     './data/data.json',
