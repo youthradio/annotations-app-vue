0ca734941e488548406178457d89c2aa8dc56430
Radames Ajna <radamajna@gmail.com>
2019-07-22 12:51:48 -0700

    fix remove brackets regex, add regex position
    add comment index position on text, useful to order comments on its
    position on text

--- scripts/get-comments.js
+++ scripts/get-comments.js
@@ -4,7 +4,7 @@ const credentials = require('./credentials.json')
 
 const DOC_ID = '1fMO25KNG1IVNHbvq5u4JOxFndr1FhN4A_2UMaEm27n0'
 
-async function getComments() {
+async function getComments () {
   const goot = new Gootenberg()
   await goot.auth.jwt(credentials)
 
@@ -15,13 +15,19 @@ async function getComments() {
     .slice(0, 2) // slice text from comments ref on bottom
     .join('')
   const textCleaned = text.replace(/[\u200B-\u200D\uFEFF]/g, '')
-  const textNoComments = textCleaned.replace(/\[.*\]/g, '') // remove comments brackets
+  const textNoComments = textCleaned.replace(/\[.\]/g, '') // remove comments brackets
   const textHtml = textNoComments.replace(/^(.*)(\r\n|\r|\n)/gm, '<p>$&</p>') // wrap lines with <p></p> tags
 
+  comments.forEach((comment, id) => {
+    const regexEscaped = comment.quotedFileContent.value.replace(/[-/\\^$*+?.()[\]{}]/g, '\\$&') // escape all caracters
+    const regex = new RegExp(regexEscaped, 'gi')
+    const match = regex.exec(textHtml)
+    comment.position = match.index
+  })
   await fsPromises.writeFile(
     './data/data.json',
     JSON.stringify({
-      comments: comments,
+      comments,
       text: textHtml
     }),
     'utf-8'
